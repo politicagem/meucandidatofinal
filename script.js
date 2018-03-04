16ab3c5314178ba459092fe8e0fdd4c4ced92092
Thais <thaissales@outlook.com>
2018-03-04 20:03:39 -0300

    Site ok

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 (function() {
   var carousel = {
     ittem: document.getElementById('carousel'),
-    interval: 2000,
-    items: this.carousel.getElementsByClassName('carousel-active'),
+    interval: 10000,
+    items: this.carousel.getElementsByClassName('carrossel-ativo'),
     itemsLength: 0,
-    controls: this.carousel.getElementsByClassName('carousel-control'),
+    controls: this.carousel.getElementsByClassName('carrossel-controle'),
     controlsLength: 0
   };
   var carouselTimer = setInterval(carouselPlay, carousel.interval);
-
+  
   carousel.itemsLength = carousel.items.length;
   carousel.controlsLength = carousel.controls.length;
-
+  
   function carouselControls() {
     for (var i = carousel.controlsLength; i--;) {
       carousel.controls[i].addEventListener('click', carouselReset, false);
     }
   }
-
+  
   function carouselPlay() {
     for (var i = carousel.itemsLength; i--;) {
       if (carousel.items[i].checked) {
@@ -39,7 +39,7 @@ function myFunction() {
       }
     }
   }
-
+  
   function carouselReset() {
     console.log(1)
     clearInterval(carouselTimer);
